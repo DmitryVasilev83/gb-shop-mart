bf66b32a4c8b92b2085e6c2b88c35868918dc573
VasilevDmitry83 <VasilevDmitry83@mail.ru>
2022-04-04 22:32:17 +0300

    Lesson_3_Vasilev_DZ

---
+++ gb-external-api/src/main/java/ru/gb/gbexternalapi/ShopConfig.java
@@ -0,0 +1,24 @@
+package ru.gb.gbexternalapi;
+
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import ru.gb.gbexternalapi.gateway.CategoryGateway;
+import ru.gb.gbexternalapi.gateway.ProductGateway;
+
+import java.util.Optional;
+
+@Configuration
+@EnableJpaAuditing(auditorAwareRef = "auditorAwareBean")
+@EnableFeignClients(clients = {CategoryGateway.class,
+        ProductGateway.class,
+        CategoryGateway.class})
+public class ShopConfig {
+
+    @Bean
+    public AuditorAware<String> auditorAwareBean() {
+        return () -> Optional.of("User");
+    }
+}
